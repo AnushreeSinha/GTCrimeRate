26520feb600ce13eddce61012b10c54dda9624eb
Yaling Wu <valence931215@gmail.com>
2016-11-09 02:03:05 -0500

    kde of scipy implemented; simple plotting using mayavi

--- kde_statsmodel.py
+++ kde_statsmodel.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy import stats
 from statsmodels.distributions.mixture_rvs import mixture_rvs
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mayavi import mlab
 
 class DataInfo:
 	COL_LAT = 12
@@ -16,12 +18,61 @@ class DataMiner:
 
 	def __init__(self):
 		self.dataset_path = "./test.csv" #"./With-Weight-Georgia_Institute_of_Technology.csv" 
-		self.formated_path = "./testout.csv"
+		self.formated_path = "./transformed.csv"
 		
 	def buildModel(self):
 #		self._convt_data()
 		self.data = self._read_formData()
 #		self._kde_statsm()
+		self._kde_scipy()
+
+	def _kde_scipy(self):
+		print "Transposed data: "
+		self.data = self.data.T
+		print(self.data)
+		#### self.data as formatted below
+		#### [[lat1,.....]
+		####  [lng1,.....] 
+		####  [hr1,......]]
+		kde = stats.gaussian_kde(self.data)
+		density = kde(self.data)
+		index = density.argsort()
+		lat, lng, hr, density = self.data[0][index], self.data[1][index], self.data[2][index], density[index]
+		print "lat: "
+		print(lat)
+		print "lng: "
+		print(lng)
+		print "hr: "
+		print(hr)
+		print "density: "
+		print(density)
+		# Evaluate kde on a grid
+#		xmin, ymin, zmin = lat.min(), lng.min(), hr.min()
+#		xmax, ymax, zmax = lat.max(), lng.max(), hr.max()
+#		xi, yi, zi = np.mgrid[xmin:xmax:30j, ymin:ymax:30j, zmin:zmax:30j]
+#		coords = np.vstack([item.ravel() for item in [xi, yi, zi]]) 
+#		density = kde(coords).reshape(xi.shape)
+
+		# Plot scatter with mayavi
+		figure = mlab.figure('DensityPlot')
+
+#		grid = mlab.pipeline.scalar_field(xi, yi, zi, density)
+#		min = density.min()
+#		max=density.max()
+#		mlab.pipeline.volume(grid, vmin=min, vmax=min + .5*(max-min))
+		
+		#### plot location points at 1:00
+		pts = mlab.points3d(lat, lng, np.ones(hr.shape), density, scale_mode='none', scale_factor=0.07)
+		mlab.axes()
+		mlab.show()
+#		fig = plt.figure()
+#		fig.suptitle('lat,lng,hr')
+#		plt.subplot(1,2,1)
+#		ax = Axes3D(fig)
+#		cset = ax.contour(lat, lng, hr, 16, extend3d=True)
+#		ax.clabel(cset, fontsize=9, inline=1)
+#		ax.scatter(x, y, z, c=density)
+#		plt.show()
 
 	def _read_formData(self):
 		form_file = open(self.formated_path,"r")
@@ -32,8 +83,10 @@ class DataMiner:
 #			print(attrs)
 			data.append(attrs)
 			record = form_file.readline()
-		print(data)
+#		print(data)
 		form_file.close()
+		data = np.array(data,dtype=float)
+#		data.astype(np.float)
 		return data
 
 	def _convt_data(self):
@@ -80,5 +133,4 @@ class DataMiner:
 		
 dm = DataMiner()
 dm.buildModel()
-#np.random.seed(12345)
-#obs_dist1 = mixture_rvs([.25,.75], size=10000, dist=[stats.norm, stats.norm],kwargs = (dict(loc=-1,scale=.5),dict(loc=1,scale=.5)))
+
