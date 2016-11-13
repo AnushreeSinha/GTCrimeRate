4ac10904dac3610772c2d199a9def8770d4ede42
Yaling Wu <valence931215@gmail.com>
2016-11-13 11:34:14 -0500

    added a subclass of scipy kde; evaluated different bandwidths for each dimension

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ A CS8803-Big Data analysis project
 - http://chrisstrelioff.ws/sandbox/2014/06/04/install_and_setup_python_and_packages_on_ubuntu_14_04.html
 
 ## The algorithm is in kde*.py:
-1. Instantiate a DataMiner object: `dm = DataMinder()`
+1. Instantiate a DataMiner object: `dm = DataMiner()`
   * converts original datasets to a new file called formatted consisting of only 3 cols of interest (done once)
   * does kde on the new formatted data file
 2. Get densities using the generated kde model: `dm.getDensity(datapoints)`
-  * datapoints should in the format like \[\[lats]\[lngs]\[hrs]] : ndarray of 3*N
+  * datapoints should in the format like \[\[lats],\[lngs],\[hrs]] : ndarray of 3*N
   * will return an np-array of 1*N
-- Public methods:
+- Public methods for DataMiner:
   * buildModel()
     - reads from formatted and does kde alg
   * convt_data()
@@ -20,7 +20,12 @@ A CS8803-Big Data analysis project
   * getDensity()
   	- return kde output densities
   	- save output along with datapoints variables
+  * selectBdwd()
+  	- a test method to select and compare bandwidths for each dimension by plotting
+- Public classes:
+  * CustBdwdKDE(dataset,bandwidth)
+  	- A Customized class inheriting stats.gaussian_kde to set fixed bandwidth for overwriting scipy's default covariance determination
 - TODO:
-  - [ ] evaluate kde outputs and selected bandwidths (currently having scatter and grid plots)
+  - [ ] evaluate kde outputs and find good bandwidths/normalize data (currently having scatter and grid plots)
   - [ ] support passing in new datasets and append the new datapoints of 3 cols to formatted.csv
   - [ ] test if the distribution is normal(gaussian)
--- kde_statsmodel.py
+++ kde_statsmodel.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
 from statsmodels.nonparametric.kernel_density import KDEMultivariate
-import numpy as np
+import numpy as np,numpy
 from scipy import stats
-from statsmodels.distributions.mixture_rvs import mixture_rvs
+#from statsmodels.distributions.mixture_rvs import mixture_rvs
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from mayavi import mlab
+#from mayavi import mlab
 
 class DataInfo:
 	COL_LAT = 12
 	COL_LONG = 13
 	COL_HR = 17	
 
+"""README.md for more details on this class"""
 class DataMiner:
 
 	DATAINFO = [12,13,17]
-	xmin, ymin = 33.7514, -84.4234
-	xmax, ymax = 33.7972, -84.3708
+	xmin, ymin = 33.773249, -84.409860#33.7514, -84.4234
+	xmax, ymax = 33.786683, -84.391138#33.7972, -84.3708
 
 	def __init__(self):
 		self.dataset_path = "./test.csv" #"./With-Weight-Georgia_Institute_of_Technology.csv" 
@@ -28,7 +29,55 @@ class DataMiner:
 		self.data = self._read_formData()
 #		self._kde_statsm()
 		self._kde_scipy()
-
+		
+#### a method to look for the right bandwidth for each dimension
+	def selectBdwd(self):
+		d = self.data
+		x, y, z = d[0],d[1],d[2] ## x=lat, y=lng, z=hr
+		xbdwd = 0.00096
+		fbdwdkde_x = CustBdwdKDE(x,xbdwd)
+		x_grid = np.linspace(x.min(), x.max(), 1000)
+		xpdf = fbdwdkde_x(x_grid)
+		xpdf_true = fbdwdkde_x(x)
+		fig, ax = plt.subplots()
+		fig.subplots_adjust(wspace=0)
+		ax.plot(x_grid, xpdf, color='blue', alpha=0.5, lw=3,label='xbw={0}'.format(xbdwd))
+		ax.plot(x,np.full(x.shape,xpdf.min()),'ro')#, c=pdf_true, alpha=0.4)
+		ax.hist(x, 20, fc='gray', histtype='stepfilled', alpha=0.3, normed=True)
+		ax.legend(loc='upper left')
+#		ax.set_xlim(self.xmin, self.xmax)
+		plt.axis([x.min(), x.max(), xpdf.min(), xpdf.max()])
+#		plt.show()
+		
+		ybdwd = 0.00096
+		fbdwdkde_y = CustBdwdKDE(y,ybdwd)
+		y_grid = np.linspace(y.min(), y.max(), 1000)
+		ypdf = fbdwdkde_y(y_grid)
+		ypdf_true = fbdwdkde_y(y)
+		fig1, ax1 = plt.subplots()
+		fig1.subplots_adjust(wspace=0)
+		ax1.plot(y_grid, ypdf, color='blue', alpha=0.5, lw=3,label='ybw={0}'.format(ybdwd))
+		ax1.plot(y,np.full(y.shape,ypdf.min()),'ro')
+		ax1.hist(y, 20, fc='gray', histtype='stepfilled', alpha=0.3,normed=True)
+		ax1.legend(loc='upper left')
+		ax1.set_xlim(y.min(), y.max())
+#		plt.show()
+		
+		
+		zbdwd = 1
+		fbdwdkde_z = CustBdwdKDE(z,zbdwd)
+		z_grid = np.linspace(z.min(), z.max(), 1000)
+		zpdf = fbdwdkde_z(z_grid)
+		zpdf_true = fbdwdkde_z(z)
+		fig2, ax2 = plt.subplots()
+		fig2.subplots_adjust(wspace=0)
+		ax2.plot(z_grid, zpdf, color='blue', alpha=0.5, lw=3,label='zbw={0}'.format(zbdwd))
+		ax2.plot(z,np.full(z.shape,zpdf.min()),'ro')#, c=pdf_true, alpha=0.4)
+		ax2.hist(z, 20, fc='gray', histtype='stepfilled', alpha=0.3, normed=True)
+		ax2.legend(loc='upper left')
+		ax2.set_xlim(z.min(), z.max())
+		plt.show()
+	
 #### takes in a time and a bunch of coordinates
 #### datapoints format: [[lats][lngs][hrs]]
 	def getDensity(self,datapoints):
@@ -94,7 +143,6 @@ class DataMiner:
 		data = []
 		while(record!=""):
 			attrs = record[:-1].split(",")
-#			print(attrs)
 			data.append(attrs)
 			record = form_file.readline()
 #		print(data)
@@ -137,15 +185,39 @@ class DataMiner:
 		#ax.hist(self.data, bins=10, normed=True, color='red')
 		#ax.plot(kde.support, kde.density, lw=2, color='black');
 		#return kde.pdf(x_grid)
+
+"""A Customized class inheriting stats.gaussian_kde to set fixed bandwidth for overwriting scipy's default covariance determination"""
+class CustBdwdKDE(stats.gaussian_kde):
+    def __init__(self, dataset, bandwidth=1):
+        self.dataset = np.atleast_2d(dataset)
+        self.d, self.n = self.dataset.shape
+        self.set_bandwidth(bandwidth)
+
+    def set_bandwidth(self, bandwidth):
+        if np.isscalar(bandwidth):
+            covariance = numpy.eye(self.d) * bandwidth**2
+        else:
+            bandwidth = numpy.asarray(bandwidth)
+            if bandwidth.shape == (self.d,): # bandwidth is 1-d array of len(self.d)
+                covariance = numpy.diag(bandwidth**2)
+            else:
+                raise ValueError("'bandwidth' should be a scalar, a 1-d array of length d.")
+        self.set_covariance(covariance)
+
+    def set_covariance(self, covariance):
+        self.covariance = numpy.asarray(covariance)
+        self.inv_cov = numpy.linalg.inv(self.covariance)
+        self._norm_factor = numpy.sqrt(numpy.linalg.det(2*numpy.pi*self.covariance)) * self.n
 		
 dm = DataMiner()
+dm.selectBdwd()
 #### grid set up, may exclude outliers
 #### plot on one plane of the hour
 xmin, ymin, zmin = 33.7514, -84.4234, 10#self.data[0].min(), self.data[1].min(), 10#self.data[2].min()
 xmax, ymax, zmax = 33.7972, -84.3708, 11#self.data[0].max(), self.data[1].max(), 11#self.data[2].max()
 lat, lng = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
 #		hr = np.mgrid[zmin:zmax:40j]
-speci_hr = 18
+speci_hr = 18 ## show data at a specific time
 hr = np.empty(lat.shape) ##set hr to a specific hour
 for i in range(lat.shape[0]):
 		for j in range(lat.shape[1]):
@@ -154,9 +226,10 @@ coords = np.vstack([item.ravel() for item in [lat, lng, hr]])
 density = dm.getDensity(coords).reshape(lat.shape)
 fig3, ax3 = plt.subplots()
 ax3.imshow(np.rot90(density), cmap=plt.cm.gist_earth_r,extent=[xmin, xmax, ymin, ymax])
-ax3.plot(dm.data[0][dm.data[2,:]==speci_hr], dm.data[1][dm.data[2,:]==speci_hr], 'k.', markersize=2)
+ax3.plot(dm.data[0][dm.data[2,:]==speci_hr], dm.data[1][dm.data[2,:]==speci_hr], 'k.', markersize=2,label='hr={0}'.format(speci_hr))
 ax3.set_xlim([xmin, xmax])
 ax3.set_ylim([ymin, ymax])
+ax3.legend(loc='upper left')
 plt.show()
 #		mlab.contour3d(lat, lng, hr, density, opacity=0.5)
 #		mlab.axes()
