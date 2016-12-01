25c03e331a45bd24bdd71c3fdbef0bc83541e9d4
Anushree Sinha <anushreesinha@anushrees-mbp.attlocal.net>
2016-12-01 00:23:00 -0500

    Rectified duplicate values of coordinates

--- wrapper.py
+++ wrapper.py
@@ -45,10 +45,10 @@ class Wrapper:
         now = datetime.datetime.now()
         hour = now.hour
         for i in range(0, 100):
-           lat_range = baselat+((toplat-baselat)*(i/10))
+           lat_range = baselat+((toplat-baselat)*(i/100))
            for j in range(0, 100):
                lats.append(lat_range)
-               lon_range = baselon+((toplon-baselon)*(j/10))
+               lon_range = baselon+((toplon-baselon)*(j/100))
                longs.append(lon_range)
                hrs.append(hour)
         datapoints = []
@@ -67,10 +67,10 @@ class Wrapper:
         now = datetime.datetime.now()
         hour = now.hour
         for i in range(0, 100):
-           lat_range = baselat+((toplat-baselat)*(i/10))
+           lat_range = baselat+((toplat-baselat)*(i/100))
            for j in range(0, 100):
                lats.append(lat_range)
-               lon_range = baselon+((toplon-baselon)*(j/10))
+               lon_range = baselon+((toplon-baselon)*(j/100))
                longs.append(lon_range)
                hrs.append(hour)
         datapoints = []
